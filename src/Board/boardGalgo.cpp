1a6a7e61758d433a877f2f13be7cfded3eaf7c44
Toreno96 <toreno96@gmail.com>
2017-03-20 13:57:13 +0100

    Fix formatting

--- src/Board/boardGalgo.cpp
+++ src/Board/boardGalgo.cpp
@@ -1,3 +1,4 @@
+
 #include "Board/boardGalgo.h"
 #include "Board/exceptions.h"
 
@@ -47,81 +48,51 @@ void BoardGalgo::setLED(int legNo, int jointNo, bool powered){
     }
 }
 
+unsigned int BoardGalgo::setPosition(int legNo, int jointNo, double angle){}
+unsigned int BoardGalgo::setPosition(int legNo, const std::vector<double>& angle){}
+unsigned int BoardGalgo::setPosition(const std::vector<double>& angle){}
 
-        unsigned int BoardGalgo::setPosition(int legNo, int jointNo, double angle){}
-        unsigned int BoardGalgo::setPosition(int legNo, const std::vector<double>& angle){}
-        unsigned int BoardGalgo::setPosition(const std::vector<double>& angle){}
+unsigned int BoardGalgo::setSpeed(int legNo, int jointNo, double speed){}
+unsigned int BoardGalgo::setSpeed(int legNo, const std::vector<double>& speed){}
+unsigned int BoardGalgo::setSpeed(const std::vector<double>& speed){}
 
-    unsigned int BoardGalgo::setSpeed(int legNo, int jointNo, double speed){}
+unsigned int BoardGalgo::setComplianceMargin(int legNo, int jointNo, double margin){}
+unsigned int BoardGalgo::setComplianceMargin(int legNo, const std::vector<double> margin){}
+unsigned int BoardGalgo::setComplianceMargin(const std::vector<double> margin){}
 
-        unsigned int BoardGalgo::setSpeed(int legNo, const std::vector<double>& speed){}
+unsigned int BoardGalgo::setComplianceSlope(int legNo, int jointNo, double slope){}
+unsigned int BoardGalgo::setComplianceSlope(int legNo, const std::vector<double>& slope){}
+unsigned int BoardGalgo::setComplianceSlope(const std::vector<double>& slope){}
 
+unsigned int BoardGalgo::setTorqueLimit(int legNo, int jointNo, double torqueLimit){}
+unsigned int BoardGalgo::setTorqueLimit(int legNo, const std::vector<double>& torqueLimit){}
+unsigned int BoardGalgo::setTorqueLimit(const std::vector<double>& torqueLimit){}
 
-        unsigned int BoardGalgo::setSpeed(const std::vector<double>& speed){}
+unsigned int BoardGalgo::readPosition(int legNo, int jointNo, double& angle){}
+unsigned int BoardGalgo::readPosition(int legNo, std::vector<double>& angle){}
+unsigned int BoardGalgo::readPosition(std::vector<double>& angle){}
 
+unsigned int BoardGalgo::readForce(int legNo, double& contactForce){}
+unsigned int BoardGalgo::readForce(const std::vector<double>& contactForce){}
 
-        unsigned int BoardGalgo::setComplianceMargin(int legNo, int jointNo, double margin){}
+unsigned int BoardGalgo::readTorqueForce(int legNo, walkers::TorqueForce& valueTF){}
+unsigned int BoardGalgo::readTorqueForce(const std::vector<double>& valueTF){}
 
+bool BoardGalgo::readContact(int legNo){}
+void BoardGalgo::readContacts(std::vector<bool>& contact){}
 
-        unsigned int BoardGalgo::setComplianceMargin(int legNo, const std::vector<double> margin){}
+unsigned int BoardGalgo::readCurrent(int legNo, int jointNo, double& servoCurrent){}
+unsigned int BoardGalgo::readCurrent(int legNo, std::vector<double>& servoCurrent){}
+unsigned int BoardGalgo::readCurrent( std::vector<double>& servoCurrent){}
 
+unsigned int BoardGalgo::readTorque(int legNo, int jointNo, double& servoTorque){}
+unsigned int BoardGalgo::readTorque(int legNo,std::vector<double>& servoTorque){}
+unsigned int BoardGalgo::readTorque(std::vector<double>& servoTorque){}
 
-        unsigned int BoardGalgo::setComplianceMargin(const std::vector<double> margin){}
+void BoardGalgo::setOffset(int legNo, int jointNo, double offset){}
+void BoardGalgo::setOffset(int legNo, const std::vector<double> offset){}
+void BoardGalgo::setOffset(const std::vector<double> offset){}
 
-        unsigned int BoardGalgo::setComplianceSlope(int legNo, int jointNo, double slope){}
-
-        unsigned int BoardGalgo::setComplianceSlope(int legNo, const std::vector<double>& slope){}
-
-        unsigned int BoardGalgo::setComplianceSlope(const std::vector<double>& slope){}
-
-        unsigned int BoardGalgo::setTorqueLimit(int legNo, int jointNo, double torqueLimit){}
-
-
-        unsigned int BoardGalgo::setTorqueLimit(int legNo, const std::vector<double>& torqueLimit){}
-
-        unsigned int BoardGalgo::setTorqueLimit(const std::vector<double>& torqueLimit){}
-
-
-        unsigned int BoardGalgo::readPosition(int legNo, int jointNo, double& angle){}
-
-        unsigned int BoardGalgo::readPosition(int legNo, std::vector<double>& angle){}
-
-        unsigned int BoardGalgo::readPosition(std::vector<double>& angle){}
-
-
-        unsigned int BoardGalgo::readForce(int legNo, double& contactForce){}
-
-        unsigned int BoardGalgo::readForce(const std::vector<double>& contactForce){}
-
-        unsigned int BoardGalgo::readTorqueForce(int legNo, walkers::TorqueForce& valueTF){}
-
-        unsigned int BoardGalgo::readTorqueForce(const std::vector<double>& valueTF){}
-
-        bool BoardGalgo::readContact(int legNo){}
-
-        void BoardGalgo::readContacts(std::vector<bool>& contact){}
-
-        unsigned int BoardGalgo::readCurrent(int legNo, int jointNo, double& servoCurrent){}
-
-        unsigned int BoardGalgo::readCurrent(int legNo, std::vector<double>& servoCurrent){}
-
-        unsigned int BoardGalgo::readCurrent( std::vector<double>& servoCurrent){}
-
-
-        unsigned int BoardGalgo::readTorque(int legNo, int jointNo, double& servoTorque){}
-
-        unsigned int BoardGalgo::readTorque(int legNo,std::vector<double>& servoTorque){}
-
-        unsigned int BoardGalgo::readTorque(std::vector<double>& servoTorque){}
-
-        void BoardGalgo::setOffset(int legNo, int jointNo, double offset){}
-
-        void BoardGalgo::setOffset(int legNo, const std::vector<double> offset){}
-
-        void BoardGalgo::setOffset(const std::vector<double> offset){}
-
-        void BoardGalgo::setDefault(void){}
-
-
+void BoardGalgo::setDefault(void){}
 
 } // namespace controller
