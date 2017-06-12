14409c04d9e532c98989214a0d753e273dcf7f0f
Marcino713 <marcin.orczyk5@gmail.com>
2017-06-12 11:54:11 +0200

    Demo 4: Sleep And Wake Up

--- main.cpp
+++ main.cpp
@@ -93,6 +93,23 @@ void Greeting(controller::BoardGalgo& bg){
                    });
 }
 
+void SleepAndWakeUp(controller::BoardGalgo& bg){
+    bg.setOperatingMode(std::vector<uint8_t>(12, 3));
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
+    bg.setSpeed(std::vector<double>(12, 0.07));
+    bg.setPosition(std::vector<double>(12, 0.0));
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
+    bg.setOperatingMode(std::vector<uint8_t>(12, 5));
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    bg.setTorqueLimit(std::vector<double>(12, 0.02));
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(1500));
+    bg.setTorqueLimit(std::vector<double>(12, 0.09));
+}
+
+
 void SetOffsets(controller::BoardGalgo& bg){
     bg.setOffset(std::vector<double>{
         0.0222482, 0.0176451, -0.313776,
@@ -111,7 +128,8 @@ int main(){
 
         //RiseOfRobot(bg);
         //Walk(bg);
-        Greeting(bg);
+        //Greeting(bg);
+        SleepAndWakeUp(bg);
 
     } catch (const std::exception& ex) {
         std::cerr << ex.what() << std::endl;
